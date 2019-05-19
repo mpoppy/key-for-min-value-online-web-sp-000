c4b9c7b067ef228b4c10fd82d0de93c53e9175c9
Marc Poppy <poppy.marc@gmail.com>
2019-05-19 17:34:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,11 +7,16 @@ ikea = {:chair => 25, :table => 85, :mattress => 450}
 def key_for_min_value(name_hash)
   min_value = Float::INFINITY
   key_for_min_value = ""
+  if name_hash = {}
+    nil
+    break
+  else
     name_hash.each do |key, value|
       if value < min_value
         key_for_min_value = key
         min_value = value
       end
     end
+  end
   key_for_min_value
 end
