7700f5e25c453e1ec4f73bacbced207b628f408a
Marc Poppy <poppy.marc@gmail.com>
2019-05-19 17:29:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,6 +10,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if value < min_value
       key_for_min_value = key
+      min_value = value
     end
   end
   key_for_min_value
