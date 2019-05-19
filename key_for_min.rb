a86d914f30c66c3755e3e7067c821280ac45f6a4
Marc Poppy <poppy.marc@gmail.com>
2019-05-19 17:17:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,11 @@ ikea = {:chair => 25, :table => 85, :mattress => 450}
 #should iterate over the hash and return the key, not the value that points to the smallest value of the set
 smallest_key = ""
 def key_for_min_value(name_hash)
-  name_hash.each do |key, value|
+  name_hash.select do |key, value|
     smallest_key = value
+    if value < smallest_key
+      smallest_key = value
+    end
   end
-
+  name_hash[smallest_key]
 end
