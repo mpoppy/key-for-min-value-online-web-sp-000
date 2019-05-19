c6b856bc6775b869cbe601b8252073ffee2db3eb
Marc Poppy <poppy.marc@gmail.com>
2019-05-19 17:25:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,6 @@ ikea = {:chair => 25, :table => 85, :mattress => 450}
 #should iterate over the hash and return the key, not the value that points to the smallest value of the set
 
 def key_for_min_value(name_hash)
-
+  min_value = Float.INFINITY
+  key_for_min_value = ""
 end
