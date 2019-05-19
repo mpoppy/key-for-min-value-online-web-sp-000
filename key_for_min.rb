37376937b45bb55cccd2b08062d41c7416bac48a
Marc Poppy <poppy.marc@gmail.com>
2019-05-19 17:31:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,12 +8,12 @@ def key_for_min_value(name_hash)
   min_value = Float::INFINITY
   key_for_min_value = ""
   if name_hash = {} nil
-  name_hash.each do |key, value|
-    if value < min_value
-      key_for_min_value = key
-      min_value = value
+    name_hash.each do |key, value|
+      if value < min_value
+        key_for_min_value = key
+        min_value = value
+      end
     end
-  end
   end
   key_for_min_value
 end
